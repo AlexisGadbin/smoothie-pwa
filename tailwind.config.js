4b7abba367ae9273395b535c9389ecea9acc16e1
AlexisGadbin <alexis@gadbin.com>
2024-09-22 00:59:02 +0200

    feat: add and configure tailwind

---
+++ tailwind.config.js
@@ -0,0 +1,23 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ['./index.html', './src/**/*.{vue,js,ts}'],
+  theme: {
+    extend: {},
+    colors: {
+      primary: {
+        50: '#EDEEFF',
+        100: '#DEE1FF',
+        200: '#C3C7FF',
+        300: '#9FA2FF',
+        400: '#8079FF',
+        500: '#6F5AFA',
+        600: '#613CEF',
+        700: '#5935D5',
+        800: '#4428AB',
+        900: '#3A2986',
+        950: '#23184E'
+      }
+    }
+  },
+  plugins: []
+}
