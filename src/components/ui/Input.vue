b5ac9037bb24de69e0c0a16650431c2d673ca87f
AlexisGadbin <alexis@gadbin.com>
2024-09-22 02:22:32 +0200

    feat: basic ui components

---
+++ src/components/ui/Input.vue
@@ -0,0 +1,32 @@
+<script lang="ts" setup>
+import { defineEmits, defineProps } from 'vue'
+
+const props = defineProps<{
+  modelValue: string | number
+  type?: 'text' | 'number' | 'email' | 'password' | 'date'
+  placeholder?: string
+  class?: string
+}>()
+
+const emit = defineEmits<{
+  (e: 'update:modelValue', value: string | number): void
+}>()
+
+const updateValue = (event: Event) => {
+  const target = event.target as HTMLInputElement
+  emit('update:modelValue', target.value)
+}
+</script>
+
+<template>
+  <input
+    :type="props.type || 'text'"
+    :placeholder="props.placeholder"
+    :value="props.modelValue"
+    :class="[
+      'w-full p-2 rounded flex items-center justify-start gap-2 border outline-none border-primary-100 text-xs focus:border-primary-700',
+      props.class
+    ]"
+    @input="updateValue"
+  />
+</template>
---
+++ src/components/ui/Select.vue
@@ -0,0 +1,31 @@
+<script setup lang="ts">
+import { defineProps } from 'vue'
+
+const props = defineProps<{
+  class?: string
+  modelValue: string
+  placeholder?: string
+}>()
+
+const emit = defineEmits<{
+  (e: 'update:modelValue', value: string): void
+}>()
+
+const updateValue = (event: Event) => {
+  const target = event.target as HTMLSelectElement
+  emit('update:modelValue', target.value)
+}
+</script>
+
+<template>
+  <select
+    :value="props.modelValue"
+    :class="[
+      'w-full p-2 text-xs focus:border-primary-700 bg-white rounded flex items-center justify-start gap-2 border border-primary-100',
+      props.class
+    ]"
+    @input="updateValue"
+  >
+    <slot />
+  </select>
+</template>
