258fe3b73bb09b019eab0dd6a68a25d51accc583
AlexisGadbin <alexis@gadbin.com>
2024-09-27 22:38:54 +0200

    feat: add home page redirection

--- src/components/AppTitle.vue
+++ src/components/AppTitle.vue
@@ -1,3 +1,15 @@
 <template>
-  <h1 class="font-black text-primary-700 text-5xl font-noto">Smoothie !</h1>
+  <button type="button" @click="handleClick">
+    <h1 class="font-black text-primary-700 text-5xl font-noto">Smoothie !</h1>
+  </button>
 </template>
+
+<script lang="ts" setup>
+import { useRouter } from 'vue-router'
+
+const router = useRouter()
+
+const handleClick = () => {
+  router.push({ name: 'home' })
+}
+</script>
